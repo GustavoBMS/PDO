72a3da2991d5db977f36d9c533dfa5cee0e7f736
Gustavo Brito Moreira dos Santos <gustavobrito.dll@gmail.com>
2019-04-26 15:47:58 -0300

    Adicionado um validador para ver se ja possui um email cadastrado no bd.

--- Conexao.php
+++ Conexao.php
@@ -3,7 +3,7 @@
 try{
     //Executando conexao, se nao conectar ele vai pro catch
     $pdo = new PDO("mysql:host=localhost;dbname=pdoyt;","root","");
-    echo "Conexão efetuada com sucesso";
+    echo "Conexão efetuada com sucesso\n";
 }catch (PDOException $e){//$e salva o erro na variavel
     echo $e->getMessage()."<br>";//mostra mensagem de erro
     echo $e->getCode()."<br>";//mostra codigo do erro
@@ -21,9 +21,17 @@ A query acima não é recomendada para ser utilizada, pois nao e segura
 $nome = "Gustavo Brito";//Variavel com os valores
 $email = "JavaeMeuOvo@gmail.com";//Variavel com os valores
 
+//Prepara o cadastro
 $insert = $pdo->prepare("INSERT INTO usuarios(nome,email)VALUES(:nome,:email)");//Query sendo preparada
-$insert->bindValue(":nome",$nome);//Seta o parametro com a variavel acima
-$insert->bindValue(":email",$email);//Seta o parametro com a variavel acima
-$insert->execute();//Executa a query
+$insert->bindValue(":nome",$nome);//Seta o parametro acima com a variavel
+$insert->bindValue(":email",$email);//Seta o parametro acima com a variavel
 
-echo $insert->rowCount();//Conta as linhas alteradas
+//Valida o cadastro
+$validar = $pdo->prepare("SELECT *FROM usuarios WHERE email=?");
+$validar->execute(array($email));//Array serve para poder entrar na interrogação
+
+if ($validar->rowCount() == 0){//0 e para saber se existe
+    $insert->execute();//Executa a query
+}else{
+    echo "Usuario com o email:$email já existe, tente outro.";
+}

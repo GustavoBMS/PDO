5ba35bffc14094cbfdb3f2276ff760795c4371a0
Gustavo Brito Moreira dos Santos <gustavobrito.dll@gmail.com>
2019-04-26 15:19:47 -0300

    Aqui mostra como que se faz um insert em uma tabela aonde eu crio variaveis com valores a serem inseridos, como fazer uma query, como utilizar o bindValue, o execute e mostrar as linhas alteradas na tabela com o rowCount.

--- Conexao.php
+++ Conexao.php
@@ -2,7 +2,7 @@
 
 try{
     //Executando conexao, se nao conectar ele vai pro catch
-    $pdo = new PDO("mysql:host=localhostt;dbname=pdoyt;","root","");
+    $pdo = new PDO("mysql:host=localhost;dbname=pdoyt;","root","");
     echo "Conexão efetuada com sucesso";
 }catch (PDOException $e){//$e salva o erro na variavel
     echo $e->getMessage()."<br>";//mostra mensagem de erro
@@ -10,3 +10,20 @@ try{
     echo $e->getFile()."<br>";//mostra o arquivo do erro
     echo $e->getLine()."<br>";//mostra a linha do erro
 }
+
+/*
+$insert=$pdo->query("SELECT * FROM usuarios WHERE idusuarios = 1");
+A query acima não é recomendada para ser utilizada, pois nao e segura
+*/
+
+//Insert \/
+
+$nome = "Gustavo Brito";//Variavel com os valores
+$email = "JavaeMeuOvo@gmail.com";//Variavel com os valores
+
+$insert = $pdo->prepare("INSERT INTO usuarios(nome,email)VALUES(:nome,:email)");//Query sendo preparada
+$insert->bindValue(":nome",$nome);//Seta o parametro com a variavel acima
+$insert->bindValue(":email",$email);//Seta o parametro com a variavel acima
+$insert->execute();//Executa a query
+
+echo $insert->rowCount();//Conta as linhas alteradas
